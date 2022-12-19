0a3d8ded0fd28e199e7dc6fce4b89da1c22f2297
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-19 18:17:37 -0500

    feat: apply rotate (#3)
    * feat: apply rotate
    
    * install scipy

--- comparemol/__init__.py
+++ comparemol/__init__.py
@@ -1,4 +1,5 @@
 from .mol import Mol
+from .rotate import get_rotatation
 from .config import set_tol
 
-__all__ = ["Mol", "set_tol"]
+__all__ = ["Mol", "set_tol", "get_rotatation"]
---
+++ comparemol/rotate.py
@@ -0,0 +1,33 @@
+"""Rotate vector."""
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+from .mol import Mol
+
+
+def get_rotatation(mol1: Mol, mol2: Mol) -> R:
+    """Get rotation: mol2 -> mol1
+    
+    Parameters
+    ----------
+    mol1 : Mol
+        molecule 1
+    mol2 : Mol
+        molecule 2
+
+    Returns
+    -------
+    scipy.spatial.transform.Rotation
+        rotation
+    """
+    assert mol1 == mol2
+    # align the atoms
+    coord1 = mol1.coord[mol1.sorted_idx]
+    coord2 = mol2.coord[mol2.sorted_idx]
+    # align the first atom
+    coord1 -= coord1[0]
+    coord2 -= coord2[0]
+
+    r, rmse = R.align_vectors(coord1, coord2)
+    assert np.isclose(rmse, 0.)
+    return r
