3adb75021dcdc67ab04b056fa1831016bc460516
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-12-21 18:16:05 -0500

    fix set_tol; improve the accuracy of comparsion (#4)
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
    
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

--- comparemol/mol.py
+++ comparemol/mol.py
@@ -1,8 +1,15 @@
 from functools import cached_property
 
 import numpy as np
-
 from numpy.typing import ArrayLike
+
+from .config import rtol, atol
+
+
+def close_index(a: ArrayLike):
+    diff = np.diff(a, prepend=0.)
+    diff_is_not_zero = ~np.isclose(diff, 0., rtol=rtol, atol=atol)
+    return np.cumsum(diff_is_not_zero)
 
 
 class Mol:
@@ -38,9 +45,14 @@ class Mol:
         Atom indexes sorted in this way:
         (1) Sort by atom type;
         (2) For the same atom type, sort by the sum of distances to other atoms.
+        (3) For the same sum of distances in tolerance, sort by sorted distance to other atoms.
         """
         sum_distance = np.sum(self.distance_matrix, axis=0)
-        return np.lexsort((sum_distance, self.types))
+        idx_sum_distance = close_index(sum_distance)
+        sorted_distance = np.sort(self.distance_matrix)
+        features = [x.ravel() for x in np.split(sorted_distance, sorted_distance.shape[1], axis=1)]
+        features = [close_index(x) for x in features]
+        return np.lexsort((*features, idx_sum_distance, self.types))
 
     @cached_property
     def sorted_distance_matrix(self) -> np.ndarray:
@@ -51,4 +63,4 @@ class Mol:
         """Check if two molecules are equal."""
         return (self.types.size == other.types.size
             and np.all(self.types[self.sorted_idx] == other.types[other.sorted_idx])
-            and np.allclose(self.sorted_distance_matrix, other.sorted_distance_matrix))
+            and np.allclose(self.sorted_distance_matrix, other.sorted_distance_matrix, rtol=rtol, atol=atol))
